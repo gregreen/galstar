7aa665e94f009f7185cdb461b5151ea0b69e9b79
Gregory Green <gregorymgreen@gmail.com>
2012-06-02 17:10:16 -0400

    Finished bash script galstar_batch.sh to run galstar on a set of input files and store input

--- scripts/galstar_batch.sh
+++ scripts/galstar_batch.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
 
 # Create an empty tar archive to store the output
-tarfn=$OUTDIR/$TAROUT
+tarfn=`readlink -m ${TAROUT%/}`
 tar --create --file=$tarfn --files-from=/dev/null
+echo "Writing binned pdfs and statistics to $tarfn."
+
+# Determine output file for std. output/error
+outerrfn=`readlink -m $OUTERR`
+outerrdir=`readlink -m ${outerrfn%/*}`
+outerrfnshort="${OUTERR##*/}"
+echo "Writing std. out/err to $outerrfn."
+
+# Determine working directory
+workingdir=`pwd`
+tmpdir=`readlink -m ${TMPDIR%/}`
 
 # Give each input file to galstar
+nfiles=`ls -l *.in | wc -l`
+counter=1
 for f in *.in; do
-	# Determine output filenames
+	# Determine filenames for galstar output
 	pixname=${f%.in}
-	statsfn="$OUTDIR/$pixname.stats"
-	binfn="$OUTDIR/${pixname}_DM_Ar.dat"
+	statsfn="$pixname.stats"
+	binfn="${pixname}_DM_Ar.dat"
 	
-	# Run galstar on input
-	echo "Running galstar with $f ..."
-	$GALSTARDIR/galstar $binfn:DM[5,20,120],Ar[0,10,400] --statsfile $statsfn --datafile $f &> $OUTDIR/out.txt
+	# Run galstar with the current l.o.s input file
+	echo "$counter of $nfiles: Running galstar with $f ..."
+	${GALSTARDIR%/}/galstar $tmpdir/$binfn:DM[5,20,120],Ar[0,10,400] --statsfile $tmpdir/$statsfn --datafile $f &>> $outerrfn
 	
-	# Compress and archive output
+	# Compress and archive output, removing temporary files
+	cd $tmpdir	# Move to temporary directory, so that the tarball has a flat directory structure
 	gzip -9 $binfn
 	tar -rf $tarfn $binfn.gz $statsfn
-	rm $binfn $statsfn
+	rm $binfn.gz $statsfn
+	cd $workingdir	# Return to the working directory
+	counter=`expr $counter + 1`
 done
+
+# Add ASCII file containing std. out/err to tar archive
+cd $outerrdir
+gzip -9 $outerrfn
+tar -rf $tarfn $outerrfnshort.gz
+rm $outerrfnshort.gz
+cd $workingdir
 
 echo "Done."

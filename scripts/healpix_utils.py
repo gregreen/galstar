f87cb595a469d107b613d416e338986258adccbe
gregreen <gregorymgreen@gmail.com>
2012-08-01 19:45:06 -0400

    Added ExtinctionMap class to healpix_utils.py

--- scripts/healpix_utils.py
+++ scripts/healpix_utils.py
@@ -28,6 +28,9 @@ import matplotlib.pyplot as plt
 import matplotlib as mplib
 import numpy as np
 import healpy as hp
+import pyfits
+
+from os.path import abspath
 
 
 def lb2thetaphi(l, b):
@@ -205,31 +208,187 @@ def healmap_to_axes(ax, m, nside, nest=True, lb_bounds=[0., 360., -90., 90.], si
 	return image
 
 
+class ExtinctionMap():
+	'''
+	Class representing a set of extinction maps at increasing distance.
+	The maps are stored internally in healpix ordering.
+	'''
+	
+	def __init__(self):
+		'''
+		Initiate a dummy extinction map with nside=1.
+		'''
+		self.nested = True
+		self.nside = 1
+		self.Ar = np.zeros((1, hp.nside2npix(self.nside)), dtype=np.float64)
+		self.mu = np.zeros(1, dtype=np.float64)
+		self.N_stars = np.zeros(hp.nside2npix(self.nside), dtype=np.uint32)
+		self.measure = np.zeros(hp.nside2npix(self.nside), dtype=np.float64)
+	
+	def __init__(self, fname, FITS=True, nside=512, nested=True):
+		if FITS:
+			self.load(fname)
+		else:
+			self.load_from_binaries(fname, nside, nested)
+		
+	
+	def save(self, fname):
+		'''
+		Save the extinction map to a FITS file.
+		'''
+		# Primary HDU with list of distance moduli
+		primary = pyfits.PrimaryHDU(self.mu)
+		primary.header.update('PIXTYPE', 'HEALPIX')
+		if self.nested:
+			primary.header.update('ORDERING', 'NESTED')
+		else:
+			primary.header.update('ORDERING', 'RING')
+		primary.header.update('NSIDE', self.nside)
+		primary.header.update('FIRSTPIX', 0)
+		primary.header.update('LASTPIX', hp.nside2npix(self.nside))
+		primary.header.update('UNITS', 'MAGNITUDES')
+		primary.header.update('NDISTANCES', self.Ar.shape[0])
+		img.header.update('DESCRIPTION', 'DIST MODULI')
+		img.header.update('DTYPE', 'FLOAT64')
+		hdu = []
+		hdu.append(primary)
+		
+		# Image HDUs with # of stars and fit measure
+		img = pyfits.ImageHDU(self.N_stars)
+		img.header.update('DESCRIPTION', 'NSTARS')
+		img.header.update('DTYPE', 'UINT32')
+		hdu.append(img)
+		img = pyfits.ImageHDU(self.measure)
+		img.header.update('DESCRIPTION', 'FIT MEASURE')
+		img.header.update('DTYPE', 'FLOAT64')
+		hdu.append(img)
+		
+		# Image HDUs with Ar
+		for mu,Ar in zip(self.mu, self.Ar):
+			img = pyfits.ImageHDU(Ar)
+			img.header.update('DESCRIPTION', 'EXTINCTION')
+			img.header.update('DTYPE', 'FLOAT64')
+			img.header.update('BAND', 'PS r')
+			img.header.update('UNITS', 'MAGNITUDES')
+			img.header.update('DIST MODULUS', mu)
+			hdu.append(img)
+		
+		hdulist = pyfits.HDUList(hdu)
+		hdulist.writeto(abspath(fname), clobber=True)
+	
+	def load(self, fname):
+		'''
+		Load an extinction map from a FITS file.
+		'''
+		try:
+			f = pyfits.open(abspath(fname))
+		except:
+			print 'Could not load %s' % fname
+			return
+		if f[0].header['ORDERING'] == 'NESTED':
+			self.nested = True
+		else:
+			self.nested = False
+		self.nside = int(f[0].header['NSIDE'])
+		self.mu = f[0].data
+		self.N_stars = f[1].data
+		self.measure = f[2].data
+		self.Ar = np.empty(int(f[0].header['NDISTANCES']), dtype=np.float64)
+		for i,img in enumerate(f[3:]):
+			self.Ar[i,:] = img.data
+		f.close()
+	
+	def load_from_binaries(self, fname, nside=512, nested=True):
+		'''
+		Load an extinction map from a set of binary files produced by
+		fit_pdfs.py.
+		'''
+		self.nside = nside
+		self.nested = nested
+		self.mu = None
+		self.Ar = None
+		self.N_stars = np.zeros(hp.nside2npix(self.nside), dtype=np.uint32)
+		self.measure = np.empty(hp.nside2npix(self.nside), dtype=np.float64)
+		self.measure.fill(np.NaN)
+		
+		if type(fname) is str:
+			fname = [fname]
+		
+		# Store (DM, Ar) fit for each healpix pixel
+		for filename in fname:
+			#print 'Opening %s ...' % filename
+			f = open(abspath(filename), 'rb')
+			while True:
+				try:	# Load in individual pixels until the EOF, or until something goes wrong
+					pix_index = np.fromfile(f, dtype=np.uint64, count=1)[0]
+					self.N_stars[pix_index] = np.fromfile(f, dtype=np.uint32, count=1)[0]
+					self.measure[pix_index] = np.fromfile(f, dtype=np.float64, count=1)[0]
+					success = np.fromfile(f, dtype=np.uint16, count=1)[0]
+					N_regions = np.fromfile(f, dtype=np.uint16, count=1)[0]
+					line_int = np.fromfile(f, dtype=np.float64, count=self.N_stars[pix_index])
+					if self.mu == None:
+						self.mu = np.fromfile(f, dtype=np.float64, count=N_regions+1)
+						self.Ar = np.empty((N_regions+1, hp.nside2npix(self.nside)), dtype=np.float64)
+						self.Ar.fill(np.NaN)
+					else:
+						mu_anchors = np.fromfile(f, dtype=np.float64, count=N_regions+1)
+					self.Ar[:, pix_index] = np.fromfile(f, dtype=np.float64, count=N_regions+1)
+				except:
+					f.close()
+					break
+			f.close()
+	
+	def evaluate(self, mu_eval):
+		'''
+		Evaluate Ar at the given distance modulus, or list of distance
+		moduli, mu_eval.
+		'''
+		if type(mu_eval) not in [list, np.ndarray]:
+			mu_eval = [mu_eval]
+		
+		# Create an empty map for each value of mu
+		Ar_map = np.empty((len(mu_eval), hp.nside2npix(self.nside)), dtype=np.float64)
+		Ar_map.fill(np.NaN)
+		
+		for k,m in enumerate(mu_eval):
+			if (m >= self.mu[0]) and (m <= self.mu[-1]):
+				for i,mu_anchor in enumerate(self.mu[1:]):
+					if mu_anchor >= m:
+						print self.mu[i], m, self.mu[i+1]
+						slope = (self.Ar[i+1] - self.Ar[i]) / (self.mu[i+1] - self.mu[i])
+						Ar_map[k] = self.Ar[i] + slope * (m - self.mu[i])
+						break
+		
+		return Ar_map
+	
+	def chi2dof(self):
+		'''
+		Return something like the chi^2/d.o.f. This is not strictly the
+		same as chi^2/d.o.f., and is not normalized, but should give a
+		measure of the relative goodness of fit in different pixels.
+		'''
+		return np.divide(self.measure, self.N_stars - len(self.mu) - 1)
+
+
+
 def main():
-	nside = 4
-	nest = True
-	npix = hp.pixelfunc.nside2npix(nside)
-	#m = np.arange(npix)
-	maps = np.random.random((6, npix))
-	
-	#hp.mollview(map=m)
-	#plt.show()
-	
-	#plt.clf()
+	fname = ['../output/gal_plane_reddening_pt1_500%d.dat' % i for i in range(3)]
+	m = ExtinctionMap(fname, FITS=False)
+	healpix_img = m.evaluate(np.linspace(5., 15., 6))
 	
 	mplib.rc('text', usetex=True)
 	mplib.rc('axes', grid=False)
 	
 	fig = plt.figure(figsize=(7,5))
 	image = None
-	for i, m in enumerate(maps):
+	for i, hpimg in enumerate(healpix_img):
 		ax = fig.add_subplot(2,3,i+1)#, projection='mollweide')
 		y, x = np.unravel_index(i, (2,3))
 		if y != 1:
 			ax.set_xticklabels([])
 		if x != 0:
 			ax.set_yticklabels([])
-		image = healmap_to_axes(ax, m, nside, nest=nest, size=(500,300), center_gal=True, clip_on=False, lb_bounds=[0., 360., -90., 90.])
+		image = healmap_to_axes(ax, hpimg, m.nside, nest=m.nested, size=(500,300), center_gal=True, clip_on=False, lb_bounds=[0., 360., -90., 90.], vmax=3.)
 	fig.subplots_adjust(wspace=0., hspace=0., right=0.88)
 	cax = fig.add_axes([0.9, 0.1, 0.03, 0.8])
 	cb = fig.colorbar(image, cax=cax)
